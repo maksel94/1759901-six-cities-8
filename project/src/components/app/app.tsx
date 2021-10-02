97e7a8417576317aeff6cb6cbbbd1bf796df575c
Dmitry Mednikov <dmednikov@micromine.com>
2021-10-02 10:26:17 +0300

    added room component

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,12 +1,13 @@
 import Favorites from '../favorites/favorites';
 import Header from '../header/header';
 import Main from '../main/main';
+import Room from '../room/room';
 import SignIn from '../sign-in/sign-in';
 
 function App(): JSX.Element {
   return <>
     <Header isMainPage={true} isAuthorized={true} />
-    <Favorites />
+    <Room />
   </>;
 }
 
