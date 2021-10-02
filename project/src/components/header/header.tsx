4f9fbd3303dd156fe302c52ceae633cf00193268
Dmitry Mednikov <dmednikov@micromine.com>
2021-10-02 11:41:16 +0300

    fixed eslint warnings

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -3,15 +3,16 @@ type HeaderProps = {isMainPage:boolean, isAuthorized: boolean}
 function Header({isMainPage, isAuthorized}: HeaderProps): JSX.Element {
   const logoStyle = isMainPage ? 'header__logo-link--active' : '';
   const href = !isMainPage ? 'main.html' : '#';
-  return <header className="header">
-    <div className="container">
-      <div className="header__wrapper">
-        <div className="header__left">
-          <a className={`header__logo-link ${logoStyle}`} href={href}>
-            <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-          </a>
-        </div>
-        {isAuthorized &&
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__left">
+            <a className={`header__logo-link ${logoStyle}`} href={href}>
+              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+            </a>
+          </div>
+          {isAuthorized &&
         <nav className="header__nav">
           <ul className="header__nav-list">
             <li className="header__nav-item user">
@@ -27,11 +28,10 @@ function Header({isMainPage, isAuthorized}: HeaderProps): JSX.Element {
               </a>
             </li>
           </ul>
-        </nav>
-        }
+        </nav>}
+        </div>
       </div>
-    </div>
-  </header>
+    </header>);
 }
 
 export default Header;
