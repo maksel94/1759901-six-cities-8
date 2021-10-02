454581f053735e58a30a83af138ab21ca500a65d
Dmitry Mednikov <dmednikov@micromine.com>
2021-10-02 10:30:50 +0300

    fixed eslint warnings

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -2,10 +2,11 @@ import Header from '../header/header';
 import Room from '../room/room';
 
 function App(): JSX.Element {
-  return <>
-    <Header isMainPage={true} isAuthorized={true} />
+  return (
+  <>
+    <Header isMainPage isAuthorized />
     <Room />
-  </>;
+  </>);
 }
 
 export default App;
