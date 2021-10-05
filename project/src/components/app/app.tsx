f0c169009c70fd31060927804e832d39e2a6fd5d
Dmitry Mednikov <dmednikov@micromine.com>
2021-10-05 17:06:39 +0300

    refactored components

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -2,19 +2,27 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import { AppRoute } from '../../constants';
 import Favorites from '../favorites/favorites';
+import Header from '../header/header';
 import Main from '../main/main';
 import NotFoundPage from '../not-fount-page/not-fount-page';
 import Room from '../room/room';
-// import Header from '../header/header';
-// import Room from '../room/room';
+import SignIn from '../sign-in/sign-in';
+
+const isAuthorized = true;
 
 function App(): JSX.Element {
+  const isMainPage: boolean = window.location.pathname === AppRoute.Main;
+  const isSignInPage: boolean = window.location.pathname === AppRoute.SignIn;
+
   return (
     <BrowserRouter>
+      {!isSignInPage && <Header isMainPage={isMainPage} isAuthorized={isAuthorized} />}
       <Switch>
-        <Route exact path={AppRoute.Main}></Route>
-        <Route exact path={AppRoute.SignIn}>
+        <Route exact path={AppRoute.Main}>
           <Main />
+        </Route>
+        <Route exact path={AppRoute.SignIn}>
+          <SignIn />
         </Route>
         <Route exact path={AppRoute.Room}>
           <Room />
--- project/src/components/not-fount-page/not-fount-page.tsx
+++ project/src/components/not-fount-page/not-fount-page.tsx
@@ -1,5 +1,15 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../constants';
+
 function NotFoundPage(): JSX.Element {
-  return <div>404 not found</div>;
+  return (
+    <div>
+      <p>404 Not Found</p>
+      <div>
+        <Link to={AppRoute.Main}>Go to Main page</Link>
+      </div>
+    </div>
+  );
 }
 
 export default NotFoundPage;
