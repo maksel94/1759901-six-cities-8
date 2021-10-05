04c76d39f15cde536aff6502e6094e4ebd29e6b8
Dmitry Mednikov <dmednikov@micromine.com>
2021-10-05 17:44:04 +0300

    added private route

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,12 +1,13 @@
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
-import { AppRoute } from '../../constants';
+import PrivateRoute from '../private-route/private-route';
 import Favorites from '../favorites/favorites';
 import Header from '../header/header';
 import Main from '../main/main';
 import NotFoundPage from '../not-fount-page/not-fount-page';
 import Room from '../room/room';
 import SignIn from '../sign-in/sign-in';
+import { AppRoute, AuthorizationStatus } from '../../constants';
 
 const isAuthorized = true;
 
@@ -24,9 +25,12 @@ function App(): JSX.Element {
         <Route exact path={AppRoute.Room}>
           <Room />
         </Route>
-        <Route exact path={AppRoute.Favorites}>
-          <Favorites />
-        </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.Favorites}
+          render={() => <Favorites />}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        />
         <Route>
           <NotFoundPage />
         </Route>
