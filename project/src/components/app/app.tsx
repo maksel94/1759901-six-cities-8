560e589f8259b679997d8834002ff7c719bee31a
Dmitry Mednikov <dmednikov@micromine.com>
2021-10-05 16:42:53 +0300

    added browser router and switch

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,12 +1,33 @@
-import Header from '../header/header';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+import { AppRoute } from '../../constants';
+import Favorites from '../favorites/favorites';
+import Main from '../main/main';
+import NotFoundPage from '../not-fount-page/not-fount-page';
 import Room from '../room/room';
+// import Header from '../header/header';
+// import Room from '../room/room';
 
 function App(): JSX.Element {
   return (
-    <>
-      <Header isMainPage isAuthorized />
-      <Room />
-    </>);
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}></Route>
+        <Route exact path={AppRoute.SignIn}>
+          <Main />
+        </Route>
+        <Route exact path={AppRoute.Room}>
+          <Room />
+        </Route>
+        <Route exact path={AppRoute.Favorites}>
+          <Favorites />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ project/src/components/not-fount-page/not-fount-page.tsx
@@ -0,0 +1,5 @@
+function NotFoundPage(): JSX.Element {
+  return <div>404 not found</div>;
+}
+
+export default NotFoundPage;
